460827bd5c3bd61d3e4ca3692c6bd5bb8e8ceb7e
Paul Fagerburg <pfagerburg@gmail.com>
2024-03-01 22:03:00 -0700

    Add unit tests for EmayFileReader
    Restructure the EmayFileReader to read from a file handle instead
    of opening a file. This allows me to write unit tests with a
    StringIO instead of needing external CSV files.
    
    Signed-off-by: Paul Fagerburg <pfagerburg@gmail.com>

--- EmayFileReader.py
+++ EmayFileReader.py
@@ -7,15 +7,17 @@
 
 """Read data files in CVS format produces by EMAY brand pulse oximeters.
 
+
 This class implements an iterator that returns tuples of a timestamp,
 an SpO2 reading, and a Pulse Rate reading.
 
 Usage example:
 ```
-emay_file = EmayFileReader("sample.csv")
+with open("sample.csv", newline="") as csvfile:
+    emay = EmayFileReader.EmayFileReader(csvfile)
 
-for val in emay_file:
-    print(str(val[0]), val[1], val[2])
+    for val in emay:
+        print(str(val[0]), val[1], val[2])
 
 2024-02-26 21:10:35 93 83
 2024-02-26 21:10:36 93 83
@@ -44,16 +46,11 @@ import sys
 
 
 class EmayFileReader:
-    def __init__(self, fname):
-        self.input_filename = fname
-        logging.debug(f"__init__, fname = '{fname}'")
-        self.csvfile = open(self.input_filename, newline="")
-        self.reader = csv.DictReader(self.csvfile)
+    def __init__(self, csvfile):
+        self.reader = csv.DictReader(csvfile)
 
         if self.reader.fieldnames != ["Date", "Time", "SpO2(%)", "PR(bpm)"]:
-            logging.error(
-                f"{self.input_filename} does not appear to be a valid EMAY CSV file"
-            )
+            logging.error(f"CSV file does not appear to be a valid EMAY CSV file")
             self.reader = None
 
     def __iter__(self):
---
+++ EmayFileReader_test.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Paul Fagerburg
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for EmayFileReader"""
+
+import datetime
+import io
+import unittest
+import EmayFileReader
+
+
+class EmayFileReaderTests(unittest.TestCase):
+    def test_bad_header(self):
+        csv = io.StringIO(
+            "Date,Time,SpO2(%),PR(bpm),Extra,Extra2\n2/26/2024,9:10:35 PM,93,83\n"
+        )
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+    def test_bad_date_time(self):
+        csv = io.StringIO("Date,Time,SpO2(%),PR(bpm)\n2/30/2024,9:10:35 PM,93,83\n")
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+    def test_missing_fields(self):
+        # We must have both SpO2 and BPM
+        csv = io.StringIO("Date,Time,SpO2(%),PR(bpm)\n2/26/2024,9:10:35 PM,93\n")
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+        csv = io.StringIO("Date,Time,SpO2(%),PR(bpm)\n2/26/2024,9:10:35 PM\n")
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+    def test_empty_fields(self):
+        # Neither SpO2 nor BPM can be empty
+        csv = io.StringIO("Date,Time,SpO2(%),PR(bpm)\n2/26/2024,9:10:35 PM,93,\n")
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+        csv = io.StringIO("Date,Time,SpO2(%),PR(bpm)\n2/26/2024,9:10:36 PM,,\n")
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+        csv = io.StringIO("Date,Time,SpO2(%),PR(bpm)\n2/26/2024,9:10:36 PM,,83\n")
+        reader = EmayFileReader.EmayFileReader(csv)
+        self.assertRaises(StopIteration, next, reader)
+
+    def test_happy_path(self):
+        csv = io.StringIO(
+            """Date,Time,SpO2(%),PR(bpm)
+2/26/2024,9:10:35 PM,93,83
+2/26/2024,9:10:39 PM,94,92
+"""
+        )
+        reader = EmayFileReader.EmayFileReader(csv)
+        row = next(reader)
+        self.assertEqual(row[0], datetime.datetime(2024, 2, 26, 21, 10, 35))
+        self.assertEqual(row[1], 93)
+        self.assertEqual(row[2], 83)
+
+        row = next(reader)
+        self.assertEqual(row[0], datetime.datetime(2024, 2, 26, 21, 10, 39))
+        self.assertEqual(row[1], 94)
+        self.assertEqual(row[2], 92)
+
+        self.assertRaises(StopIteration, next, reader)
+
+
+if __name__ == "__main__":
+    unittest.main()
